360a08506b1ef7e3c606071af9bd78155eb7a7c1
Manpreet Kaur <manu04251992@gmail.com>
2025-07-10 10:29:49 -0700

    update

--- models/Form.js
+++ models/Form.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose');
 
 const formSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: String,
-  fields: [Object], // Customize fields as needed
-  createdAt: { type: Date, default: Date.now },
+  title: String,
+  fields: Array
 });
 
 module.exports = mongoose.model('Form', formSchema);
+

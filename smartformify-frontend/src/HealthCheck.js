d5e2ff9035e2f05bc97739f89761f67d9b5dbef5
Manpreet Kaur <manu04251992@gmail.com>
2025-07-11 22:36:50 -0700

    UPDATE AND SAVE

---
+++ smartformify-frontend/src/HealthCheck.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HealthCheck = () => {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Health Check</h1>
+      <p>SmartFormify frontend is running smoothly! 🚀</p>
+    </div>
+  );
+};
+
+export default HealthCheck;
